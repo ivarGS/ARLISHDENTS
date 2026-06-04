4fe2fa3fa8540dbf8b2c24efb61834289f49f321
ivarGS <ivargradysarfaraz@gmail.com>
2026-06-04 08:57:34 +0700

    Add files via upload

---
+++ Project1.js
@@ -0,0 +1,17 @@
+const foto = document.querySelectorAll("img");
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+foto.forEach(img => {
+  img.onclick = function () {
+    img.classList.toggle("besar");
+  };
+});
+
+if (menuToggle && navLinks) {
+  menuToggle.onclick = function () {
+    navLinks.classList.toggle("open");
+    const expanded = navLinks.classList.contains("open");
+    menuToggle.setAttribute("aria-expanded", expanded);
+  };
+}
